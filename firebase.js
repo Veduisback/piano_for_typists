0faecf672572d8da002177930c5484049e025410
Vedang Jaiswal <veduisback@gmail.com>
2026-05-19 23:38:13 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,19 +8,12 @@ import { getAuth }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyBMwku1ayb7Xsx-ZP6twXnYnIbhhZgb2WM",
-
   authDomain: "pianofortypist.firebaseapp.com",
-
   projectId: "pianofortypist",
-
   storageBucket: "pianofortypist.firebasestorage.app",
-
   messagingSenderId: "239631451993",
-
   appId: "1:239631451993:web:4bebe5ff8799c35ca66e48",
-
   measurementId: "G-W1TLTJT9V6"
 };
 
@@ -30,4 +23,4 @@ const db = getFirestore(app);
 
 const auth = getAuth(app);
 
-export { db, auth };
+export { auth, db };
